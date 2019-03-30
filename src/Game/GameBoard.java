b69622f8b9873458563b14dc9b9f1634881c6384
Grant <grantl@sfu.ca>
2019-03-30 15:13:11 -0700

    added square ID and mouse event skeleton

--- src/Game/GameBoard.java
+++ src/Game/GameBoard.java
@@ -1,12 +1,14 @@
 package Game;
 
 import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 import constraints.Drawable;
 import controller.GameState;
 
-public class GameBoard extends Drawable{
+public class GameBoard extends Drawable implements MouseListener{
 	//Individual Squares
 	private ArrayList<Square> squareList = new ArrayList<Square>();
 	//number of Rows
@@ -16,22 +18,63 @@ public class GameBoard extends Drawable{
 	//size of squares in pixels
 	private int sizeOfSquares = 30;
 	public GameBoard() {
-		for(int y = 0; y < columns; y++) {
-			for(int x = 0; x < rows; x++) {
+		int squareID = 0;
+		for(int x = 0; x < rows; x++) {
+			for(int y = 0; y < columns; y++) {
 				//add square to squareList
-				squareList.add(new Square(GameState.numberOfPlayers, GameState.territoryLimit, GameState.penThickness, sizeOfSquares*x, sizeOfSquares*y, sizeOfSquares));
+				squareList.add(new Square(GameState.numberOfPlayers, squareID, GameState.territoryLimit, GameState.penThickness, sizeOfSquares*x, sizeOfSquares*y, sizeOfSquares));
+				squareID++;
 			}
+		
 		}
 	}
 	@Override
 	public void render(Graphics2D g) {
-
+		//for each square in the gameboard
+		for(Square square : squareList) {
+			//render the square
+			square.render(g);
+		}
 		
 	}
 
 	@Override
 	public void tick() {
 
+		
+	}
+	//Mouse click happened
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		//For each square in the gameboard
+		for(Square square : squareList) {
+			//check if it was the one clicked
+			if(square.isClicked(e)) {
+				//The square has been clicked
+				//get the square id and gamestate->connectionEndpoint->server to let them know of this update
+				square.getSquareID();
+			}
+		}
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 

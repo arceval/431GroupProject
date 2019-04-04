ce903108dbeaa6698753e1af78da927e763ac1cd
Grant <grantl@sfu.ca>
2019-04-04 03:44:27 -0700

    added Drawable comments

--- src/constraints/Drawable.java
+++ src/constraints/Drawable.java
@@ -3,9 +3,9 @@ package constraints;
 import java.awt.Graphics2D;
 
 public abstract class Drawable {
-	
+//Render method if page contains drawables	
 public abstract void render(Graphics2D g);
-
+//tick method for logic update
 public abstract void tick();
 
 
